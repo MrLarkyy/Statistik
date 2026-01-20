11cb763b79851548b283e7cfe281075ef6a4f52b
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-20 05:46:43 +0100

    Add BlockBreakStatistic for tracking block break events

---
+++ src/main/kotlin/gg/aquatic/statistik/impl/BlockBreakStatistic.kt
@@ -0,0 +1,29 @@
+package gg.aquatic.statistik.impl
+
+import gg.aquatic.common.argument.ObjectArgument
+import gg.aquatic.common.argument.impl.PrimitiveObjectArgument
+import gg.aquatic.statistik.ListenerStatisticType
+import gg.aquatic.statistik.StatisticAddEvent
+import org.bukkit.entity.Player
+import org.bukkit.event.block.BlockBreakEvent
+
+object BlockBreakStatistic: ListenerStatisticType<Player>() {
+    override val arguments: Collection<ObjectArgument<*>> = listOf(
+        PrimitiveObjectArgument("types", ArrayList<String>(), true)
+    )
+
+    override fun createListener() = listen<BlockBreakEvent> {
+        val player = it.player
+        for (statisticHandle in handles) {
+            val args = statisticHandle.args
+            val types = args.stringCollection("types") ?: listOf()
+
+            if ("ALL" !in types && it.block.type.name.uppercase() !in types) {
+                continue
+            }
+
+            val event = StatisticAddEvent(this, 1, player)
+            statisticHandle.consumer(event)
+        }
+    }
+}
