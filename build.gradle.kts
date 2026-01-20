07e16bdc8ec11d364a5bde6da5382248b04f8d5f
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-20 05:30:15 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
+    `maven-publish`
+}
+
+group = "gg.aquatic"
+version = "26.0.1"
+
+repositories {
+    maven("https://repo.nekroplex.com/releases")
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("gg.aquatic:Common:26.0.5")
+    compileOnly("gg.aquatic:KRegistry:25.0.1")
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+val maven_username = if (env.isPresent("MAVEN_USERNAME")) env.fetch("MAVEN_USERNAME") else ""
+val maven_password = if (env.isPresent("MAVEN_PASSWORD")) env.fetch("MAVEN_PASSWORD") else ""
+
+publishing {
+    repositories {
+        maven {
+            name = "aquaticRepository"
+            url = uri("https://repo.nekroplex.com/releases")
+
+            credentials {
+                username = maven_username
+                password = maven_password
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.aquatic"
+            artifactId = "Kurrency"
+            version = "${project.version}"
+            from(components["java"])
+        }
+    }
+}
