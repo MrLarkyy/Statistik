6f4cee9803ae21c937b4c5c8d2eb3b074fcb6ecc
MrLarkyy_ <studiotadikl@gmail.com>
2026-02-18 23:26:31 +0100

    Support bulk statistic registration during Statistik initialization

--- src/main/kotlin/gg/aquatic/statistik/Statistik.kt
+++ src/main/kotlin/gg/aquatic/statistik/Statistik.kt
@@ -4,12 +4,12 @@ import gg.aquatic.kregistry.bootstrap.BootstrapHolder
 
 internal lateinit var bootstrapHolder: BootstrapHolder
 
-fun initializeStatistik(bootstrapHolder: BootstrapHolder) {
+fun initializeStatistik(bootstrapHolder: BootstrapHolder, statistics: HashMap<String, StatisticType<*>>) {
     gg.aquatic.statistik.bootstrapHolder = bootstrapHolder
 
     StatistikRegistryHolder.registryBootstrap(bootstrapHolder) {
         registry(StatisticType.REGISTRY_KEY) {
-
+            statistics.forEach { add(it.key, it.value) }
         }
     }
 }
